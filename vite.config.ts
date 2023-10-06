f7dc27441b121a3f65696bffa2b11a7ecc5c6d83
KoluaGylchyk <43492309+KoluaGylchyk@users.noreply.github.com>
2023-10-06 16:36:11 +0300

    ok

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,5 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: process.env.NODE_ENV === "production" ? "/Fullstack-js-tack/" : "/",
+  base: "/Fullstack-js-tack/",
 });
